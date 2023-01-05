90e5e0bfcf4ef071b0a6fc1dcabc884aa0747c0f
miasK3011 <neemiascalebe2014@gmail.com>
2023-01-05 18:12:28 -0300

    Melhorias de código

--- Implementação/Histograma.py
+++ Implementação/Histograma.py
@@ -6,7 +6,7 @@ class Histograma():
         self.matrix = matrix
         self.x, self.y = x, y
         self.pixels_totais = x*y
-
+        
     def run(self):
         """ 
         Executa o algoritmo que plota o histograma, histograma normalizado e histograma acumulado
@@ -15,8 +15,8 @@ class Histograma():
         for i in range(self.x):
             for j in range(self.y):
                 self.bins[self.matrix[i, j]] += 1
-
-        cdf, pdf = self.normalizar()
+        
+        pdf = self.normalizar()
         
         plt.figure('Histograma')
         plt.bar(range(256), self.bins)
@@ -25,7 +25,7 @@ class Histograma():
         plt.bar(range(256), pdf)
         
         plt.figure('Histograma Acumulado')
-        plt.bar(range(256), cdf)
+        plt.bar(range(256), self.cdf_values)
         
         plt.show()
 
@@ -36,22 +36,23 @@ class Histograma():
         pdf = [bin / self.pixels_totais for bin in self.bins]
 
         cdf = 0
-        cdf_values = [0] * 256
+        self.cdf_values = [0] * 256
         for i in range(256):
             cdf += pdf[i]
-            cdf_values[i] = cdf
+            self.cdf_values[i] = cdf
+            
+        return pdf
         
-        return cdf_values, pdf
 
     def create_image(self):
-        
-        pdf = self.normalizar()
-
+        """ 
+        Cria uma nova imagem utilizando o novo histograma
+        """
         image = Image.new("L", (self.x, self.y))
         pixels = image.load()
         
         for i in range(self.x):
             for j in range(self.y):
-                pixels[i, j] = int(pdf[self.matrix[i, j]] * 255)
+                pixels[i, j] = int(self.cdf_values[self.matrix[i, j]] * 255)
 
         return image
